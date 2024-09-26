73997ed2e366d601643a472cd89c6f2e61f106b9
bgdnyrmln <bgdnyrmln@gmail.com>
2024-09-26 16:56:36 +0300

    commit
    zxc

--- pages/script.js
+++ pages/script.js
@@ -16,5 +16,38 @@ function onEntry(entry) {
   }
 
 
+  const headerline = document.getElementById("headerline");
+  const viewportHeight = window.innerHeight;
+  const scrollLimit = 0.99 * viewportHeight;
 
+  // Add an event listener for scroll event
+  window.addEventListener("scroll", function() {
+      // Check if page has been scrolled by at least 1 pixel
+      if (window.scrollY > 0) {
+        headerline.style.backgroundColor = "black"; // Change to desired color
+      } else {
+        headerline.style.backgroundColor = "transparent";  // Original color
+      }
+
+      if (scrollY > scrollLimit) {
+        headerline.style.opacity = "0"; // Hide the div by making it transparent
+        headerline.style.pointerEvents = "none"; // Disable interaction when hidden
+      } else {
+        headerline.style.opacity = "1";  // Show the div
+        headerline.style.pointerEvents = "auto"; // Re-enable interaction
+    }
+    
+  });
   
+
+
+
+  $( ".changetheme" ).on("click", function() {
+    if( $( "body" ).hasClass( "dark" )) {
+        $( "body" ).removeClass( "dark" );
+        $( ".changetheme" ).text( "unfreak my eyes pls" );
+    } else {
+        $( "body" ).addClass( "dark" );
+        $( ".change" ).text( "ON" );
+    }
+});
