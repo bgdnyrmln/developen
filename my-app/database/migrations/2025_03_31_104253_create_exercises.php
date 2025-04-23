33167583292b98281f6a744c9f7e5e45cf5eebab
bgdnyrmln <bgdnyrmln@gmail.com>
2025-04-23 15:06:43 +0300

    now fs

--- my-app/database/migrations/2025_03_31_104253_create_exercises.php
+++ my-app/database/migrations/2025_03_31_104253_create_exercises.php
@@ -23,7 +23,7 @@ return new class extends Migration
         Schema::create('exercises', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->string('description', 10000);
             $table->string('answer');
             $table->timestamps();
         });
